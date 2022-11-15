1698b36e0b288c5c3c2d3b0825d5edd23f035945
Gizem MERCAN <98174724+gizemmercant@users.noreply.github.com>
2022-11-15 11:16:09 +0300

    Add files via upload

---
+++ love_calculator.py
@@ -0,0 +1,17 @@
+name1 = input("Your name:")
+name2 = input("Girlfriend-Boyfriend name:")
+
+love = len(name1) + len(name2)
+
+if len(name1) > len(name2):
+    love -= 5
+
+else:
+    love += 3
+
+love *= 42
+love = love / (100 + len(name2))
+
+love = 10 if love > 10 else round(love,0)
+
+print("{} and {} score is {} out of 10".format(name1, name2, love))
